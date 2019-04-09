7a43997700bb924a3701f18c53ff93d432a4d1cd
hhhoang <thimyhanh.hoang@gmail.com>
2019-04-09 14:54:18 +0200

    Complete Day 16

--- Day16.py
+++ Day16.py
@@ -23,3 +23,42 @@ So although each player must play every day, there can be particular pairs of pl
 
 It is not necessary to consider the case where the number of golfers is zero; no tests will check for that. If you do wish to consider that case, note that you should accept as valid all possible solutions for zero golfers, who (vacuously) can indeed play in an unlimited number of groups of zero.
 """
+
+def valid(a):
+    result = 0
+    players_per_day = []
+    groups = []
+    group_len = []
+    groups_len_per_day = []
+    for day in a:
+        if ''.join(sorted(''.join(day))) not in players_per_day:
+            players_per_day.append(''.join(sorted(''.join(day))))
+        if len(day) not in groups_len_per_day:
+            groups_len_per_day.append(len(day))
+        for group in day:
+            if len(group) not in group_len:
+                group_len.append(len(group))
+            groups.append(group)
+
+    # each golfer plays exactly once every day
+    if len(players_per_day) != 1:
+        result += 1
+
+    # number and size of groups the same every day
+    if len(groups_len_per_day) != 1 or len(group_len) != 1:
+        result += 1
+
+    # each player plays with every other at most once
+    for i in range(len(groups)):
+        for j in range(len(groups)):
+            if j != i:
+                intersection = set(groups[i]).intersection(set(groups[j])) 
+                if len(intersection) >= 2:
+                    result += 1
+    if result == 0:
+        return True
+    else:
+        return False
+    
+
+
