ae11d47230d7d46b0a1fdae50cda37e2cbfc56d4
hhhoang <thimyhanh.hoang@gmail.com>
2019-04-19 22:05:53 +0200

    Complete Day 19

--- Day19.py
+++ Day19.py
@@ -1,1 +1,14 @@
 # https://www.codewars.com/kata/complementary-dna/train/python
+def DNA_strand(dna):
+    result = ""
+    for elem in dna:
+        print(elem)
+        if elem == "A":
+            result += "T"
+        elif elem == "T":
+            result += "A"
+        elif elem == "G":
+            result += "C"
+        elif elem == "C":
+            result += "G"
+    return result
