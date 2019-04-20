f1ae314561d24dc63a0ced7d87d39f18eea8651f
hhhoang <thimyhanh.hoang@gmail.com>
2019-04-20 07:24:21 +0200

    Create Day 21

---
+++ Day21.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/after-midnight/train/python?
+"""
+Write a function that takes a negative or positive integer, 
+which represents the number of minutes before (-) or after (+) Sunday midnight, 
+and returns the current day of the week and the current time in 24hr format ('hh:mm') as a string.
+"""
