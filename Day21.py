7e57b28b34e6ab810afe6f6964cf3af33a36e6a7
hhhoang <thimyhanh.hoang@gmail.com>
2019-04-21 15:07:11 +0200

    update Day 21
    day of negative value is not right

--- Day21.py
+++ Day21.py
@@ -4,3 +4,19 @@ Write a function that takes a negative or positive integer,
 which represents the number of minutes before (-) or after (+) Sunday midnight, 
 and returns the current day of the week and the current time in 24hr format ('hh:mm') as a string.
 """
+def day_and_time(mins):
+    dayspos = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    print(mins)
+    minutes, hours_final, days_final = get_min_hour_day(mins)
+    if mins == 0:
+        return 'Sunday 00:00'
+    elif mins > 0:
+        return "{} {}:{}".format(dayspos[days_final], str(hours_final).zfill(2),str(minutes).zfill(2))
+    elif mins < 0:
+        return "{} {}:{}".format(dayspos[-days_final], str(24-hours_final).zfill(2),str(minutes).zfill(2))
+def get_min_hour_day(mins, clock_wise=True):
+    hours, minutes = divmod(mins, 60)
+    days, hours_final = divmod(hours, 24)
+    days_final = days%7
+    print(days_final, days, hours_final, hours, minutes)
+    return(minutes, hours_final, days_final)
